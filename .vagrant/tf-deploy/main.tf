a8b79a555de718daf83474e1422c00f1c7f8a3cb
NyoomNyoom <jacksonnorth1275@gmail.com>
2023-10-03 09:28:28 +1300

    Making the terraform file

---
+++ .vagrant/tf-deploy/main.tf
@@ -0,0 +1,79 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow inbound SSH traffic"
+
+  ingress {
+    description = "SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web_server" {
+  ami           = "ami-010e83f579f15bba0"
+  instance_type = "t2.micro"
+  key_name      = "cosc349-2023"
+
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt update
+              sudo apt install -y apache2
+              sudo systemctl start apache2
+              sudo systemctl enable apache2
+              EOF
+
+  tags = {
+    Name = "WebServer"
+  }
+}
+
+resource "aws_instance" "Admin_server" {
+    ami           = "ami-010e83f579f15bba0"
+    instance_type = "t2.micro"
+    key_name      = "cosc349-2023"
+    
+    vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+    
+    user_data = <<-EOF
+                #!/bin/bash
+                sudo apt update
+                sudo apt install -y apache2
+                sudo systemctl start apache2
+                sudo systemctl enable apache2
+                EOF
+    
+    tags = {
+        Name = "AdminServer"
+    }
+}
+
+resource "aws_db_instance" "LollystoreDB" {
+    allocated_storage    = 20
+    engine               = "mysql"
+    engine_version       = "5.7"
+    instance_class       = "db.t2.micro"
+    username             = "admin"
+    password             = "password"
+    parameter_group_name = "default.mysql5.7"
+    publicly_accessible = true
+    skip_final_snapshot = true
+}
+
+output "web_server_ip" {
+  value = aws_instance.web_server.public_ip
+}
