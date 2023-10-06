3d0e2531bb2643df90385b97c15999a01a25d144
NyoomNyoom <jacksonnorth1275@gmail.com>
2023-10-06 21:35:33 +1300

    Prepping for submission

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
-define('DB_HOST', 'terraform-20231005234020300200000001.clsautq0toao.us-east-1.rds.amazonaws.com:3306');
+define('DB_HOST', 'terraform-20231006072035546700000003.clsautq0toao.us-east-1.rds.amazonaws.com:3306');
 define('DB_PORT', '3306');
 define('DB_NAME', 'LollyStore');
 define('DB_USER', 'admin');
 define('DB_PASS', 'password');
-define('STORE_URL', 'http://34.204.78.184');
-define('ADMIN_URL', 'http://34.229.41.18');
+define('STORE_URL', 'http://ec2-18-234-151-142.compute-1.amazonaws.com');
+define('ADMIN_URL', 'http://ec2-54-224-48-122.compute-1.amazonaws.com');
 ?>
