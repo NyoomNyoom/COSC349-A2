f4b8b856f20385dfae9bc2f2edd50e9bb4da6519
NyoomNyoom <jacksonnorth1275@gmail.com>
2023-10-06 14:56:24 +1300

    Config.php setup with the dynamic data

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+define('DB_HOST', 'terraform-20231005234020300200000001.clsautq0toao.us-east-1.rds.amazonaws.com:3306');
+define('DB_PORT', '3306');
+define('DB_NAME', 'LollyStore');
+define('DB_USER', 'admin');
+define('DB_PASS', 'password');
+define('STORE_URL', 'http://34.204.78.184');
+define('ADMIN_URL', 'http://34.229.41.18');
+?>
